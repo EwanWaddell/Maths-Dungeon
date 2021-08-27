410c7e7e730545710f4c40ac63ee3b28adbf6d77
EwanWaddell <87102914+EwanWaddell@users.noreply.github.com>
2021-08-28 00:54:14 +1200

    2nd version of UserInput
    Can now allow any capital letter variation of Yes and No (although currently isn't programmed to do anything with No)

--- UserInput.cs
+++ UserInput.cs
@@ -7,13 +7,18 @@ public class UserInput : MonoBehaviour
 {
     public GameObject Canvas;
     public InputField inputField;
+    public GameObject HaveYouUsedScreen;
     public GameObject HowToUseScreen;
 
     public void GetInput() {
-        if(inputField.text == "yes")
+        if(inputField.text == "yes" || inputField.text == "Yes" || inputField.text == "YES" || inputField.text == "yES" || inputField.text == "YEs" || inputField.text == "YeS" || inputField.text == "yEs" || inputField.text == "yeS")
         {
-        HowToUseScreen.SetActive(false);
+        HaveYouUsedScreen.SetActive(false);
         Canvas.SetActive(true);
+        }
+        if(inputField.text == "no" || inputField.text == "NO" || inputField.text == "No" || inputField.text == "nO")
+        {
+            Debug.Log("No");
         }
         else
         {
